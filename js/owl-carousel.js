59f1abf815ba773170aa00ae8cf35161500f9de0
Nursultan <nursultan.sharsheev@gmail.com>
2019-12-27 00:30:03 +0600

    first commit

---
+++ js/owl-carousel.js
@@ -0,0 +1,153 @@
+$('#owl-carousel-main').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('#owl-carousel-main-1').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('#owl-carousel-main-2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('#owl-carousel-main-3').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('#owl-carousel-main-4').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+$('#carousel-portfolio').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+
+$('#carousel-portfolio2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["<i class=\"fas fa-chevron-left\"></i>","<i class=\"fas fa-chevron-right\"></i>"],
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplaySpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
